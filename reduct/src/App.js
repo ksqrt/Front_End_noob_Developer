6867464cad37c466e9d1c61d6a8316407c04e3cd
ksqrt <ksqrt98@gmail.com>
2022-03-24 10:13:06 +0900

    리덕스 설치

--- reduct/src/App.js
+++ reduct/src/App.js
@@ -1,59 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-class AddNumber extends Component {
-  render() {
-    return (
-      <div>
-       <h1>Add Number</h1>
-       <input type="button" value="+"></input> 
-       <input type="text" value="0"></input> 
-      </div>
-    )
-  }
-}
-class AddDisplay extends Component {
-  render() {
-    return (
-      <div>
-       <h1>Dislay Number</h1>
-       <input type="text" value="0"></input> 
-      </div>
-    )
-  }
-}
+class App extends Component {
+  state = { number: 0 };
 
-class AddNumberRoot extends Component {
   render() {
     return (
       <div>
-        <h1>Add Number Root</h1>
-        <AddNumber></AddNumber>
+        <h1>Root</h1>
+        <AddNumberRoot
+          onClick={function (size) {
+            this.setState({ number: this.state.number + size });
+          }.bind(this)}
+        ></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
       </div>
     );
   }
-}
-
-class DisplayNumberRoot extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Display Number Root</h1>
-        <AddDisplay></AddDisplay>
-      </div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
-    </div>
-  );
 }
 
 export default App;
