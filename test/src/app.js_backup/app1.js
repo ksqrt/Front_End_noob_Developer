277ab905d62ccce868203c8b516152c000ff0757
ksqrt <ksqrt98@gmail.com>
2022-04-02 15:35:47 +0900

    app.js 백업입니다

---
+++ test/src/app.js_backup/app1.js
@@ -0,0 +1,11 @@
+// import "./App.css";
+import React, { useEffect, useState, useCallback } from "react";
+import { useMemo } from "react";
+
+export default function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
