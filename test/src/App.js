b9be78d8e991776b069231b752c4cd0aa6160947
ksqrt <ksqrt98@gmail.com>
2022-04-03 01:07:44 +0900

    aa

--- test/src/App.js
+++ test/src/App.js
@@ -1,69 +1,15 @@
-// import "./App.css";
-import React, { useEffect, useState, useCallback } from "react";
-import { useMemo } from "react";
+import React from "react";
+import "./App.css";
 
-function Box({ createBoxStyle }) {
-  const [style, setStyle] = useState({});
+function App() {
+  const a = "hello";
 
-  useEffect(() => {
-    console.log("박스 키우기");
-    setStyle(createBoxStyle());
-  }, [createBoxStyle]);
-
-  return <div style={style}></div>;
-}
-
-export default function App() {
-  const [toggle,setToggle] = useState(true);
-  const [number,setNumber] = useState(0);
-  // useCallback
-  // 메모이제이션을 실행
-  const someFunc = useCallback(() => {
-    console.log("number : "+number);
-    return;
-  },[number]);
-  const [size, setSize] = useState(100);
-  const [isDark, setIsDark] = useState(false);
-
-  //use 콜백으로 재사용 방지 !!! 효율적인 실행
-  const createBoxStyle = useCallback(() => {
-    return {
-      backgroundColor: "pink",
-      width: `${size}px `,
-      height: `${size}px `,
-    };
-  }, [size]);
-
-  useEffect(() => {
-    console.log("첫실행 호출, somefunction 호출");
-  }, []);
-
-  // useMemo 는 메모이제이션 을 사용한다
-  // 두번째 인자의 배열안의 state 가 변경될때만 !! 실행
-  // const hardSum = hardCalculation(hardNumber);
   return (
-    <div style={{ background: isDark ? "black" : "white" }}>
-      <div id="grid">
-        <input type="number" value ={number} onChange={(e)=>{setNumber(e.target.value)}}></input>
-        <button onClick={someFunc}>call some Func</button>
-        <button onClick={()=>{setToggle(!toggle)}}>{toggle.toString()}</button>
-        <br></br>
-        <input
-          type="number"
-          value={size}
-          onChange={(e) => {
-            setSize(e.target.value);
-          }}
-        ></input>
-        <button
-          onClick={() => {
-            setIsDark(!isDark);
-          }}
-        >
-          Change theme
-        </button>
-        <Box createBoxStyle={createBoxStyle}></Box>
-      </div>
+    <div>
+      <p>와우</p>
     </div>
   );
 }
+
+export default App;
+
