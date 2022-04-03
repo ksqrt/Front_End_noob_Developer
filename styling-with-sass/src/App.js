ee187b586c09baab0b177365266f29243beed59d
ksqrt <ksqrt98@gmail.com>
2022-04-03 15:10:00 +0900

    투두 리스트

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,58 +1,25 @@
-import React from 'react';
-import './App.scss';
-import Button from './components/Button';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate.js";
+import TodoHead from "./components/TodoHead.js";
+import TodoList from "./components/TodoList.js";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button size="large" onClick={() => console.log('클릭됐다!')}>
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button color="gray">BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button color="pink">BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="blue" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline>
-          BUTTON
-        </Button>
-        <Button size="small" color="pink" outline>
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" fullWidth>
-          BUTTON
-        </Button>
-        <Button size="large" color="gray" fullWidth>
-          BUTTON
-        </Button>
-        <Button size="large" color="pink" fullWidth>
-          BUTTON
-        </Button>
-      </div>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead></TodoHead>
+        <TodoList></TodoList>
+      </TodoTemplate>
+      
+    </>
   );
 }
 
