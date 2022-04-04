ef363cc5b448c4ff631179efe59b1c2d7232452d
ksqrt <ksqrt98@gmail.com>
2022-04-04 16:09:52 +0900

    라우터 설치

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import Users from './User';
+import React from "react";
+import Users from "./User";
 
 function App() {
-  return <Users />;
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  const getDog = async () => {
+    await sleep(1000);
+    return '멍멍이';
+  };
+  
+  const getRabbit = async () => {
+    await sleep(500);
+    return '토끼';
+  };
+  const getTurtle = async () => {
+    await sleep(3000);
+    return '거북이';
+  };
+  
+  async function process() {
+    const first = await Promise.race([
+      getDog(),
+      getRabbit(),
+      getTurtle()
+    ]);
+    console.log(first);
+  }
+  
+  process();
+
+  return (
+    <div>
+      <Users />;
+    </div>
+  );
 }
 
 export default App;
