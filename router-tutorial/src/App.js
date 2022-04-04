620375f9a0d45fc3d2655de85bc99a0fd41c799f
ksqrt <ksqrt98@gmail.com>
2022-04-04 19:21:02 +0900

    af

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,32 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 export default function App() {
+
+
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+        <Link to="/profiles/velopert">프로파일러</Link>
+        </li>
+      </ul>
+      <hr></hr>
+      {/* 라투터들 안에 라우터만 넣자.. 앵간하면 */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" component={Profile}></Route>
       </Routes>
+      
     </div>
   );
 }
