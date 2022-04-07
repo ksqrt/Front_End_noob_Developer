013676de1f5f1f1d37d000034abde3cc571269b2
ksqrt <ksqrt98@gmail.com>
2022-04-07 16:36:21 +0900

    d

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import logger from "redux-logger"
+// import myLogger from "./middlewares/myLogger";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer, applyMiddleware(logger)); // 여러개의 미들웨어를 적용 할 수 있습니다.
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
-ReactDOM.render(
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
 );
+
+reportWebVitals();
