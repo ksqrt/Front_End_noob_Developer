9ca4d0f71ca672dc21d222607357bea6f5818e8c
ksqrt <ksqrt98@gmail.com>
2023-05-21 20:08:44 +0900

    express 모듈 설치

--- nodetest/index.js
+++ nodetest/index.js
@@ -8,3 +8,12 @@ figlet("sunguk World!!", function (err, data) {
   }
   console.log(data);
 });
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
